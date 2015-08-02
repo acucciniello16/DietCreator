e03a249aef462702f44fc197ce6a52a5b61fabc6
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2015-08-01 23:43:50 -0400

    Adding files to create a bulking diet, bulkingDiet.cpp, bulkingDiet.h, main.cpp(took the place of cuttingDriver.cpp)

---
+++ bulkingDiet.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#ifndef BULKINGDIET_H_INCLUDED
+#define BULKINGDIET_H_INCLUDED
+using namespace std;
+
+	double* bulkingDiet();
+
+#endif // BULKINGDIET_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include "cuttingDiet.h"
+#include "bulkingDiet.h"
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string.h>
+
+using namespace std;
+
+int main ()
+{
+	double* dietPointer;
+	string goal;
+	cout << "What do you want to do to get to your goal physique? (Gain or Loose)" << endl;
+	cin >> goal;
+
+	if (goal == "Gain")
+	{
+		dietPointer = bulkingDiet();
+		cout << "Your Total amount of Daily Calories is: " << dietPointer[0] << endl;
+		cout << "Your Total amount of Daily Calories is (in protein): " << dietPointer[1] << endl;
+		cout << "Your Total amount of Daily Calories is (in fat): " << dietPointer[2] << endl;
+		cout << "Your Total amount of Daily Calories is (in carbohydrates): " << dietPointer[3] << endl;
+		cout << "Your Total amount of Daily Grams of protein is: " << dietPointer[4] << endl;
+		cout << "Your Total amount of Daily Grams of fat is: " << dietPointer[5] << endl;
+		cout << "Your Total Amount of Daily Grams of carbohydrates is: " << dietPointer[6] << endl;
+	}
+	
+	else if (goal == "Loose")
+	{
+		dietPointer = cuttingDiet();
+		cout << "Your Total amount of Daily Calories is: " << dietPointer[0] << endl;
+		cout << "Your Total amount of Daily Calories is (in protein): " << dietPointer[1] << endl;
+		cout << "Your Total amount of Daily Calories is (in fat): " << dietPointer[2] << endl;
+		cout << "Your Total amount of Daily Calories is (in carbohydrates): " << dietPointer[3] << endl;
+		cout << "Your Total amount of Daily Grams of protein is: " << dietPointer[4] << endl;
+		cout << "Your Total amount of Daily Grams of fat is: " << dietPointer[5] << endl;
+		cout << "Your Total Amount of Daily Grams of carbohydrates is: " << dietPointer[6] << endl;
+	}
+	
+	else 
+	{
+		cout << "Please try to rerun entering 'Gain' or 'Loose' " << endl;
+	}
+
+return 0;
+}
