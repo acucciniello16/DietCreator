9538c82addf259c0229bd02a1e9eee2956a4517f
ashtonhunter <huntera2@tcnj.edu>
2015-09-22 16:13:21 -0400

    script now only compiles once, checks for output file

--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
+if [ ! -f run.o ]; 
+	then
+		#File not found
+		echo Compiling...
+		g++ bulkingDiet.cpp cuttingDiet.cpp main.cpp -o run.o
+		echo -----------Ready-------------
+		./run.o
+	else
+		#File found
+		./run.o
+fi
 
-echo Compiling...
-g++ bulkingDiet.cpp cuttingDiet.cpp main.cpp -o run.o
-echo -----------Ready-------------
-./run.o
-rm -r run.o
